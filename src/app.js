37ca385a89f63c8f948050967676a31806411704
maweber98 <weberdesignstudio@gmail.com>
2021-02-09 14:40:38 -0500

    cleaned up console logs

--- src/app.js
+++ src/app.js
@@ -8,28 +8,3 @@ import './styles/styles.scss';
 console.log('Indecision App!.....');
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-// class OldSyntax {
-//     constructor() {
-//         this.name = 'Mike';
-//         this.getGreeting = this.getGreeting.bind(this);
-//     }
-//     getGreeting() {
-//         return `Hi my name is ${this.name}`;
-//     }
-// }
-
-// const olderSyntax = new OldSyntax();
-// const greeting = olderSyntax.getGreeting;
-// console.log(greeting());
-
-// class NewSyntax {
-//     name = 'Jennifer';
-//     getGreeting = () => {
-//         return `Hi my name is ${this.name}`
-//     }
-// }
-
-// const newerSyntax = new NewSyntax();
-// const newGreeting = newerSyntax.getGreeting;
-// console.log(newGreeting());
